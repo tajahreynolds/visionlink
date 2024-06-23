50546d013bf0b652a0420332d5c83d36e1adfe91
TaJah Reynolds <tajahreynolds22@gmail.com>
2024-06-23 17:02:58 -0400

    register relative points component

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,7 +23,10 @@ Vue.prototype.$http = axios;
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
+Vue.component(
+  "relative-points",
+  require("./components/RelativePoints.vue").default
+);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
