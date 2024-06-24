f79b733a85e92ec3078eb3fcfd4c2f4bc2d26a6a
TaJah Reynolds <tajahreynolds22@gmail.com>
2024-06-24 04:27:37 -0400

    match seed data to exampel data

--- database/seeders/PointTableSeeder.php
+++ database/seeders/PointTableSeeder.php
@@ -15,10 +15,11 @@ class PointTableSeeder extends Seeder
     public function run()
     {
         DB::table('point')->insert([
-            ['name' => 'Origin', 'x' => 0, 'y' => 0],
-            ['name' => 'Point A', 'x' => 0, 'y' => 1],
-            ['name' => 'Point B', 'x' => 1, 'y' => 0],
-            ['name' => 'Point C', 'x' => 2, 'y' => 2],
+            ['name' => 'A', 'x' => 0, 'y' => 1],
+            ['name' => 'B', 'x' => 1, 'y' => 0],
+            ['name' => 'C', 'x' => 1, 'y' => 1],
+            ['name' => 'D', 'x' => 2, 'y' => 2],
+            ['name' => 'E', 'x' => 3, 'y' => 3],
         ]);
     }
 }
